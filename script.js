5b864c69b22ca21fdc85fe505922648039aca12b
Fapxt-Cxt <68070079@kmitl.ac.th>
2025-10-06 15:16:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-const myinput = document.getElementById("myinput")
-let current_value = 1
+let count = 0
+function change() {
+    const account = document.getElementById("account").value;
+    const cash = document.getElementById("cash").value;
+    
+    count = count + 1
+    const outputMassage = (count + ",Current account balance: " + account + ", Current cash balance: " + cash);
+    const outputElement = document.getElementById("output");
+    const existingOutput = outputElement.value;
+    outputElement.value = outputMassage + "\n" + existingOutput;
+}
 
-function add(x) {
-    current_value += Number(x)
-    myinput.value = current_value
+function proceed() {
+    const cash = document.getElementById("cash");
+    const account = document.getElementById("account");
+    const oper = document.getElementById("oper");
+    const bankoper = document.getElementById("bankoper")
+
+    count = count + 1
+    const outputElement = document.getElementById("output");
+    if (bankoper.value === "Deposit") {
+        if (Number(cash.value) < Number(oper.value)) {
+            const outputMassage = (count + ", Couldn't deposit entered balance. (Insufficient cash balance)");
+            const existingOutput = outputElement.value;
+            outputElement.value = outputMassage + "\n" + existingOutput;
+        } else {
+            account.value = Number(account.value) + Number(oper.value);
+            cash.value = Number(cash.value) - Number(oper.value);
+            const outputMassage = (count + ", Current account balance: " + account.value + ", Current cash balance: " + cash.value);
+            const existingOutput = outputElement.value;
+            outputElement.value = outputMassage + "\n" + existingOutput
+        }
+    } else if (bankoper.value === "Withdraw") {
+        if (Number(account.value) < Number(oper.value)) {
+            const outputMassage = (count + ", Couldn't withdraw entered balance. (Insufficient account balance)");
+            const existingOutput = outputElement.value;
+            outputElement.value = outputMassage + "\n" + existingOutput;
+        } else {
+        account.value = Number(account.value) - Number(oper.value);
+        cash.value = Number(cash.value) + Number(oper.value);
+        const outputMassage = (count + ", Current account balance: " + account.value + ", Current cash balance: " + cash.value);
+        const existingOutput = outputElement.value;
+        outputElement.value = outputMassage + "\n" + existingOutput;
+        } 
+    }
 }
